2bf37a2175a6b82711ad8a414ea0c7f4b09870f0
Qin (Roy) Luo <r.luo2418@gmail.com>
2021-07-02 15:34:47 +1000

    finish tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react' 
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>Hi I am the author of this page.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
